c19613794ff7c3fa8622011098121576229ab693
vorobevjlt540 <jlt540e@gmail.com>
2024-04-27 22:56:44 +0500

    loop fall object

--- main.py
+++ main.py
@@ -1,2 +1,73 @@
-import sys
-print(sys.version)
+from tkinter import *
+import random
+import time
+def left(event):
+    x =- 10
+    y = 0
+    canvas.move(present_unit, x, y)
+
+def right(event):
+    x =+ 10
+    y = 0
+    canvas.move(present_unit, x, y)
+
+def up(event):
+    x = 0
+    y =- 10
+    canvas.move(present_unit, x, y)
+
+def down(event):
+    x = 0
+    y =+ 10
+    canvas.move(present_unit, x, y)
+
+def move_forw():
+    x = 0
+    y =+ 10
+    canvas.move(present_unit, x, y)
+
+WIDTH = 400
+HEIGHT = 600
+BACKGROUND_COLOR = "#31554f"
+x = 0
+y = 0
+
+root = Tk()
+
+root.geometry("400x600")
+ 
+canvas = Canvas(root, bg=BACKGROUND_COLOR, height=HEIGHT, width=WIDTH)
+
+present_unit = canvas.create_rectangle(x, y, x + 20, y + 20, fill="#80CBC4")
+
+canvas.pack()
+
+global inc
+
+def move_start():
+    for _ in range(10):  
+        x =+ 1
+        y =+ 1
+        time.sleep(1)
+        canvas.move(present_unit, y, x)
+    
+
+def move_stop():
+    root.after_cancel(move_stop)
+
+
+move_start()
+
+# canvas.bind_all('<KeyPress-Down>', move_start)
+# canvas.bind_all('<KeyRelease-Down>', move_stop)
+
+# root.bind('<Left>', left)
+
+# root.bind('<Right>', right)
+
+# root.bind('<Up>', up)
+
+# root.bind('<Down>', down)
+
+if __name__ == "__main__":
+    root.mainloop()
