4206cfc085c61f7ca54ddedb7d12903b000c9254
vorobevjlt540 <jlt540e@gmail.com>
2024-04-30 22:13:28 +0500

    Реворк логики отслеживания структуры поверхности
    Добавлены: функция распознавания состояния клетки, функция замены состояния клетки, комментарии к структуре

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from tkinter import *
 import random
-cell = [25, 45, 65, 85, 105, 125, 145, 165, 185, 205]
-apearCell = [85, 105, 125]
-speed = 10
+from bisect import bisect
+# Клетки по оси Х
+x_cell = [25, 45, 65, 85, 105, 125, 145, 165, 185, 205]
+# Клетки по оси Y
+y_cell = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640]
+apearCell = [165,185,205]
+speed = 1
 class Position:
     def __init__(self, x = 0, y = 0):
         self.x = random.choice(apearCell)
         self.y = y
-        self.cell = {25:600, 45:600, 65:600, 85:600, 105:600, 125:600, 145:600, 165:600, 185:600, 205:600}
+        # Игровое поле ввиде матрицы значений (True, False)
+        # Каждый ключ это одно из значений y_cell
+        # Ключ указывает на список состоящий из словарей, 
+        # каждый отдельный словарь в этом списке, 
+        # хранит состояние поля на который указывает ключ (одно из значений x_cell)
+        self.field = {20: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 40: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 60: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 80: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 100: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 120: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 140: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 160: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 180: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 200: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 220: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 240: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 260: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 280: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 300: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 320: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 340: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 360: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 380: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 400: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 420: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 440: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 460: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 480: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 500: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 520: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 540: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 560: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 580: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 600: [{25: False}, {45: False}, {65: False}, {85: False}, {105: False}, {125: False}, {145: False}, {165: False}, {185: False}, {205: False}], 620: [{25: True}, {45: True}, {65: True}, {85: True}, {105: True}, {125: True}, {145: True}, {165: True}, {185: True}, {205: True}]}
+
+    def get_cell_condition(self, y, x):
+        idx = x_cell.index(x)
+        # Возвращаем состояние отдельно взятой клетки от всего поля
+        # Используя строго заданные координаты 
+        # по (x_cell, y_cell), как входные параметры
+        return self.field[y][idx][x]
+    
+    def change_condition(self, begin_x, end_x, begin_y, end_y):
+        x_coor = []
+        y_coor = []
+        for i in range(begin_x, end_x, 20):
+            x_coor.append(i)
+        for j in range(begin_y, end_y, -20):
+            y_coor.append(j)
+        for y in y_coor:
+            for x in x_coor:
+                idx = x_cell.index(x)
+                self.field[y][idx][x] = True
+            
 
 class Form:
     def __init__(self, shape_x = 0, shape_y = 0):
@@ -18,15 +47,14 @@ class Ground:
     def __init__(self, shape_x = 0):
         self.height = 0
 
-
 w = 225
 h = 600
 b = "#31554f"
 unit = Position()
 unit_shape = Form()
 field_ground = Ground()
-lay_ground = 0
-save_right = 0
+shape_x = 0
+shape_y = 0
 root = Tk()
 root.geometry("400x600")
 canvas = Canvas(root, bg=b, height=h, width=w)
@@ -35,83 +63,34 @@ canvas.pack()
 def move_start():
     unit.y = unit.y + 1
     canvas.delete("s_unit")
-    lay_ground = unit.x + unit_shape.shape_x
-    matchedKeys = [unit.cell[k] for k in unit.cell.keys() if k >= unit.x and k < (unit.x + unit_shape.shape_x)]
-    field_ground.height = min(matchedKeys)
-    print(field_ground.height)
+    shape_x = unit.x + unit_shape.shape_x
+    shape_y = unit.y - unit_shape.shape_y
     position = canvas.create_rectangle(
             unit.x, unit.y, 
-            lay_ground,
-            unit.y - unit_shape.shape_y, 
+            shape_x,
+            shape_y, 
             fill="#80CBC4",
             tags="s_unit"
             )
-        
     repeat = root.after(speed, move_start)
-
-    if unit.y >= field_ground.height or (unit.y - unit_shape.shape_y) >= field_ground.height:
+    idx = bisect(y_cell, unit.y)
+    if unit.get_cell_condition(y_cell[idx], unit.x):
         position = canvas.create_rectangle(
             unit.x, unit.y, 
-            lay_ground, 
-            unit.y - unit_shape.shape_y,
+            shape_x,
+            shape_y,
             fill="#80CBC4",
             tags="stay_unit"
             )
-        
+        unit.change_condition(unit.x, shape_x, unit.y, shape_y)
+        for k, v in unit.field.items():
+            print(k, v)        
         root.after_cancel(repeat)
-        cellValues = []
-        matchedKeys = [k for k in unit.cell.keys() if k >= unit.x and k < (unit.x + unit_shape.shape_x)]
-        for i in matchedKeys:
-            unit.cell[i] = unit.cell[i] - unit_shape.shape_y
-            cellValues.append(unit.cell[i])
-        for i in matchedKeys:
-            unit.cell[i] = min(cellValues)
         unit.y = 0
         unit.x = random.choice(apearCell)
-        move_start()       
-
-def up(event):
-    if unit.x > cell[-5] and unit_shape.shape_y == 80:
-        unit.x = cell[-5]
-        unit_shape.shape_x = 80
-        unit_shape.shape_y = 20
-    elif unit.x <= cell[-5] and unit_shape.shape_y == 80:
-        unit_shape.shape_x = 80
-        unit_shape.shape_y = 20
-    else:
-        unit_shape.shape_x = 20
-        unit_shape.shape_y = 80
-
-def left(event):
-    idx = cell.index(unit.x)
-    if idx == 0:
-        unit.x = cell[idx]
-    else:    
-        unit.x = cell[idx - 1]
-
-def right(event):
-    idx = cell.index(unit.x) 
-    if idx == len(cell)-2:
-        unit.x = cell[idx]
-    elif (unit.x + unit_shape.shape_x > cell[-1]) and (unit_shape.shape_y == 80):
-        unit.x = cell[-2]
-    elif (unit.x + unit_shape.shape_x > cell[-2]) and (unit_shape.shape_y == 20):
-        unit.x = cell[-5]        
-    else:    
-        unit.x = cell[idx + 1]
-
-def speedUp(event):
-    unit.y = field_ground.height - 1
-
+        move_start()          
+        
 move_start()
-
-root.bind('<Up>', up)
-
-root.bind('<Right>', right)
-
-root.bind('<Left>', left)
-
-root.bind('<Down>', speedUp)
 
 if __name__ == "__main__":
     root.mainloop()
